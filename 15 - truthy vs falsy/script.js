ecabd885b1cff647c16a251e4b3a4f76c16bd374
Bunleap Beun <bunleap997@gmail.com>
2024-12-21 23:48:21 +0700

    truthy vs falsy

---
+++ 15 - truthy vs falsy/script.js
@@ -0,0 +1,18 @@
+// truthy vs falsy
+
+let x = 1 > 0; // true
+let y = false;
+
+// ! the different between truthy and falsy
+// ? truthy: true when the variable have value
+// ? falsy: false when the variable non value
+
+let a = 'Apple';
+let b;
+let c = 1;
+
+if (a) {
+    console.log('This is true.');    
+} else {
+    console.log('This is falsy.');
+}
