76c5a675e70184e6c32325d87fd368c529603ae1
Bunleap Beun <bunleap997@gmail.com>
2024-12-21 13:12:40 +0700

    datatype

---
+++ 02. variable/script.js
@@ -0,0 +1,29 @@
+// console.log('Nice to see you Bunleap.');
+// console.log('Nice to see you Alex.');
+
+// variable
+
+// let greeting = 'Hello my name is: ';
+// let myName = "Bunleap";
+
+// console.log(greeting + myName + ' ' + "and i am a chill guy.");
+
+// var, let, const
+
+// var: is for old school, and it's globle scope, can reassign value, var can create double the same name of variable
+// let: is for modern, and it's block scope, can reassign value, let can not create double the name of variable
+// const: is for modern, and it's block scope, can not reassign value
+
+// var
+// var country = "Cambodia";
+// country = "USA";
+
+// let
+// let country = "Cambodia";
+// country = "USA";
+
+// const: can not reassign value
+// const country = "Cambodia";
+// country = "USA";
+
+// console.log(country);
---
+++ 03 - data type/script.js
@@ -0,0 +1,47 @@
+// Data Type
+
+// string & number
+
+
+// string
+
+const text = "Hello welcome to Cambodia!";
+var animal = "it's a Cat.";
+var animal = 'it\'s a Cat.';
+
+// template string
+var animal = `it's a Cat`;
+
+let greeting = "Welcome to ";
+let country = "Cambodia";
+
+// console.log(text);
+// console.log(animal);
+
+// use template
+// console.log(`${greeting} ${country}, Kingdom Of Wonder.`);
+
+
+// number
+
+let num = '10'; // it will be string you can get it to calculate
+
+let num1 = 10;
+let num2 = 30;
+
+const result = num1 + num2;
+
+// modulus
+
+const modulus = 9 % 4; // output: 1 (Jek yk som nol)
+
+let calcu = 10 + 5 * 10; // output: 60, becuz it use math rule for cal as nomarl
+
+calcu = (10 + 5) * 10; // output: 150, becuz we want it cal plus first by use ()
+
+
+console.log(calcu);
+
+
+
+
