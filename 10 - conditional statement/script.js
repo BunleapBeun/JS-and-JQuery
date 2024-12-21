4b65f90d5c0d53637151db0dc0b71982bce16597
Bunleap Beun <bunleap997@gmail.com>
2024-12-21 15:51:08 +0700

    condition statement

---
+++ 10 - conditional statement/script.js
@@ -0,0 +1,29 @@
+// Conditional statement
+// >, <, >=, <=, ==, ===, !, !=, !==
+// if, else if, else
+
+// for the condition statement, its work only with true condition
+
+// use with boolean
+// if (false) {
+//     console.log("Hello World.");
+// } else if (true) {
+//     console.log('This is: else if');
+// } else {
+//     console.log('This is else.');
+    
+// }
+
+const num1 = 10;
+const num2 = '10';
+
+if (num1 > num2) {
+    console.log("Hello World.");
+}
+
+// for == it compare with value, but only with string number it will true, but with string text it will false
+console.log(num1 == num2);
+
+// for === or (!==) it compare with type if it different type but the same value it still flase
+console.log(num1 === num2);
+
