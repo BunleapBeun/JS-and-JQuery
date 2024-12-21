65fb14707e965b3958dee00c0b3a159a8dfc38ac
Bunleap Beun <bunleap997@gmail.com>
2024-12-21 23:53:45 +0700

    ternary operator

---
+++ 16 - ternary operator/script.js
@@ -0,0 +1,16 @@
+// ternary operator
+
+// ! normal condition
+// if (true) {
+//     console.log(true);
+// }
+// else {
+//     console.log(false);
+// }
+
+// ! ternary operator
+
+const a = false;
+    // true return                  // false ruturn 
+a ? console.log('This is true.') : console.log('This is false');
+
