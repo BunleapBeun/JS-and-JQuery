5f8838ea05c267e744bf2a80952b24cb7b84f3c9
Bunleap Beun <bunleap997@gmail.com>
2024-12-22 11:29:36 +0700

    dom window object

---
+++ 26 - DOM - window object/script.js
@@ -0,0 +1,17 @@
+// window object
+// document
+
+// console.log(window); // check all respresent browser and in window have document
+
+// console.log(window.document);
+// ! cuz document is properties direct from window we use only document without window
+// console.log(document);
+
+
+
+// ! check all method and properties
+// console.dir(document);
+
+
+alert('Hello World.');
+
