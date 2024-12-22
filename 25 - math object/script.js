b93d1f636fea038e96245b0610b1a7ca5fece048
Bunleap Beun <bunleap997@gmail.com>
2024-12-22 11:19:56 +0700

    math

---
+++ 25 - math object/script.js
@@ -0,0 +1,14 @@
+// math built-in object
+// floor ceil sqrt min max random
+
+var num = 12.343;
+
+var result = Math.floor(num); // cut float
+var result = Math.ceil(num); // add one, cut float
+var result = Math.ceil(num); // add one, cut float
+var result = Math.sqrt(num); // sqrt root
+var result = Math.min(5, 10, 2, 45, 3); // get smallest value
+var result = Math.max(5, 10, 2, 45, 3); // get largest value
+var result = Math.random(100); // random num while refresh
+
+console.log(result);
