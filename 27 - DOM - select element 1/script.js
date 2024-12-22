8bd2ef66fe31237bd70aba84bc58ecbd271d2c1d
Bunleap Beun <bunleap997@gmail.com>
2024-12-22 13:52:49 +0700

    select by id and select by query

---
+++ 27 - DOM - select element 1/script.js
@@ -0,0 +1,16 @@
+// select element from javascript
+// return node object
+//getElementById and querySelector
+
+// ! 1
+// const element = document.body; // return node obj
+// console.log(element); // ouput: node obj
+
+// const element = document.getElementById('heading'); // its select by id
+// console.log(element);
+
+const element = document.querySelector('.list .item'); // it is select like select class or id in css
+
+console.log(element);
+
+
