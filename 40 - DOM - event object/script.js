bdade161c82471482c509110444ed0e0c3eee66e
Bunleap Beun <bunleap997@gmail.com>
2024-12-23 22:09:44 +0700

    event obj

---
+++ 40 - DOM - event object/script.js
@@ -0,0 +1,18 @@
+// event object as arg
+// even.type
+// even.preventDefaul() // use for stop refresh while click
+// even.target
+
+const h2 = document.querySelector('h2');
+const a = document.querySelector('a');
+
+// h2.addEventListener('click', function (e) {
+//     console.log(e);
+// });
+
+
+//? even.preventDefaul()
+// a.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     // console.log(e);
+// });
